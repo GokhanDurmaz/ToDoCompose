6a42776a13abf5d8c3a919cf00ab663327ddc1ed
gokhandurmaz <gdurmaz1234@gmail.com>
2026-03-13 05:41:35 +0300

    fix(gradle-setup): extract common android configurations to convention plugin.

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -26,24 +26,6 @@ androidSecrets {
 
 android {
     namespace = "com.flowintent.network"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
 
     buildFeatures {
         buildConfig = true
